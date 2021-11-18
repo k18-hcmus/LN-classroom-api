c6a03601d115f32fe2a977b92f0c0920a3783cf4
Trần Thành Long <longtt2705@gmail.com>
2021-11-19 00:06:19 +0700

    Implement invite with mail via intite link (#6)

--- src/routes/classroom.route.ts
+++ src/routes/classroom.route.ts
@@ -1,11 +1,15 @@
 import { Router } from 'express';
-import { createClassroom, getAllClassroom } from '@controllers/classroom.controller';
-
+import { createClassroom, getAllClassroom, getInviteLink, inviteToClassromByEmail, joinClassByLink } from '@controllers/classroom.controller';
+import authenticateJWT from '@middlewares/jwt-auth.mdw';
 
 // User-route
 const router = Router();
+router.use(authenticateJWT)
 
 router.get('/', getAllClassroom);
 router.post('/', createClassroom);
+router.get('/invitation', getInviteLink);
+router.post('/invitation', joinClassByLink);
+router.post('/send-invitation', inviteToClassromByEmail);
 
 export default router;
